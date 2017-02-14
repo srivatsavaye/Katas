10752040082ef55fc68d03cb4977fdec661ecad4
Raju Srivatsavaye <rsrivatsavaye@smg.com>
2017-02-14 14:39:06 -0600

    Initial commit

---
+++ Katas.Tests/KataTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katas.Tests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void SumWithOutHighestAndLowestNumber()
+        {
+            var kata = new Kata();
+            Assert.AreEqual(16, kata.SumWithOutHighestAndLowestNumber(new[] { 6, 2, 1, 8, 10 }));
+            Assert.AreEqual(6, kata.SumWithOutHighestAndLowestNumber(new[] { 1, 1, 11, 2, 3 }));
+        }
+    }
+}
---
+++ Katas/Kata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class Kata
+    {
+
+        public int SumWithOutHighestAndLowestNumber(int[] numbers)
+        {
+            if (numbers == null || numbers.Length <= 1)
+                return 0;
+            numbers = numbers.OrderBy(r => r).ToArray();
+            numbers[0] = 0;
+            numbers[numbers.Length - 1] = 0;
+            //var orderedList = numbers.OrderBy(n => n).ToList();
+            //orderedList.RemoveAt(0);
+            //orderedList.RemoveAt(orderedList.Count - 1);
+
+            return numbers.Sum(r => r);
+        }
+    }
+}
