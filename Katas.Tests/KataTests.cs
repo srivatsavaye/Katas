9601fba62c17958aca1822251aa47341063a6e4b
Raju Srivatsavaye <srivatsavaye@gmail.com>
2018-09-21 16:53:57 -0500

    Added DeNico

--- Katas.Tests/KataTests.cs
+++ Katas.Tests/KataTests.cs
@@ -6,10 +6,77 @@ namespace Katas.Tests
     [TestClass]
     public class KataTests
     {
+        private readonly Kata kata = new Kata();
+        [TestMethod]
+        public void DeNico()
+        {
+            Assert.AreEqual("secretinformation", kata.DeNico("crazy", "cseerntiofarmit on  "));
+            //Assert.AreEqual("secretinformation", kata.DeNico("crazy", "cseerntiofarmit on"));
+            //Assert.AreEqual("abcd", kata.DeNico("abc", "abcd"));
+            //Assert.AreEqual("1234567890", kata.DeNico("ba", "2143658709"));
+            //Assert.AreEqual("message", kata.DeNico("a", "message"));
+            //Assert.AreEqual("key", kata.DeNico("key", "eky"));
+        }
+
+        [TestMethod]
+        public void IsMerge()
+        {
+            Assert.AreEqual(true, kata.isMerge("Bahamas", "Bahas", "Bananas from am"));
+        }
+
+        [TestMethod]
+        public void IsAllPossibilities()
+        {
+            Assert.AreEqual(false, kata.IsAllPossibilities(null));
+            Assert.AreEqual(false, kata.IsAllPossibilities(new int[] { }));
+            Assert.AreEqual(false, kata.IsAllPossibilities(new[]
+            {
+                0,
+                2,
+                19,
+                4,
+                4,
+                3,
+                2,
+                1,
+                0,
+                3,
+                2,
+                10,
+                4,
+                1,
+                6,
+                1,
+                1,
+                8,
+                3,
+                1,
+                1,
+                0,
+                1,
+                2,
+                3,
+                1,
+                2,
+                3,
+                4,
+                0,
+                -1,
+                2,
+                3,
+                0,
+                2,
+                3,
+                0
+            }));
+            //Assert.AreEqual(false, kata.IsAllPossibilities(new[]{ 6, 2, 4, 2, 2, 2, 1, 5, 0, 0 }));
+
+            Assert.AreEqual(false, kata.IsAllPossibilities_copied(new[] { 6, 2, 4, 2, 2, 2, 1, 5, 0, 0 }));
+        }
+
         [TestMethod]
         public void SumWithOutHighestAndLowestNumber()
         {
-            var kata = new Kata();
             Assert.AreEqual(16, kata.SumWithOutHighestAndLowestNumber(new[] { 6, 2, 1, 8, 10 }));
             Assert.AreEqual(6, kata.SumWithOutHighestAndLowestNumber(new[] { 1, 1, 11, 2, 3 }));
         }
@@ -17,7 +84,6 @@ namespace Katas.Tests
         [TestMethod]
         public void FlipBit()
         {
-            var kata = new Kata();
             Assert.AreEqual(7, kata.FlipBit(15, 4));
             Assert.AreEqual(1, kata.FlipBit(0, 1));
             Assert.AreEqual(14, kata.FlipBit(15, 1));
@@ -28,7 +94,6 @@ namespace Katas.Tests
         [TestMethod]
         public void BouncingBall()
         {
-            var kata = new Kata();
             Assert.AreEqual(3, kata.BouncingBall(3.0, 0.66, 1.5));
             Assert.AreEqual(15, kata.BouncingBall(30.0, 0.66, 1.5));
         }
@@ -36,7 +101,6 @@ namespace Katas.Tests
         [TestMethod]
         public void BuyingACar()
         {
-            var kata = new Kata();
             int[] r = new int[] { 6, 766 };
             var actualResult = kata.BuyingACar(2000, 8000, 1000, 1.5);
             Assert.AreEqual(6, actualResult[0]);
--- Katas/Kata.cs
+++ Katas/Kata.cs
@@ -2,12 +2,150 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Katas
 {
     public class Kata
     {
+        public string DeNico(string key, string message)
+        {
+           /*
+            int[] coder = key.OrderBy(x => x).Select(e => key.IndexOf(e)).ToArray();
+            return string.Concat(message.Select((e, i) => message[i / key.Length * key.Length + Array.IndexOf(coder, i % key.Length)])).TrimEnd(' '); ;
+
+            Console.WriteLine(string.Join(",",message.Select((e, i) => message[i / key.Length * key.Length + Array.IndexOf(coder, i % key.Length)])));
+
+            var test = message.Select((e, i) =>
+            {
+                Console.WriteLine(message[i / key.Length * key.Length + Array.IndexOf(coder, i % key.Length)]);
+                return message[i / key.Length * key.Length + Array.IndexOf(coder, i % key.Length)];
+            });
+
+
+            */
+            var splitKey = Regex.Split(key, string.Empty).Where(s => s != string.Empty).OrderBy(s => s).ToArray();
+
+            var splitMessage = Regex.Split(message, string.Empty).Where(s => s != string.Empty).ToArray();
+            var k = 0;
+            
+            foreach (var m in splitMessage)
+            {
+                if (k < splitKey.Length)
+                {
+                    splitKey[k] = splitKey[k] + m;
+                    k++;
+                }
+                else
+                {
+                    splitKey[0] = splitKey[0] + m;
+                    k = 1;
+                }
+            }
+
+            //Console.WriteLine(key);
+            //Console.WriteLine(message);
+            //Console.WriteLine(string.Join(",", splitKey));
+            
+            var finalResult = new StringBuilder();
+
+            var max = splitKey.Max(s => s.Length);
+            for (var i = 1; i < max; i++)
+            {
+                foreach (var e in key)
+                {
+                    var t = splitKey.First(s => s.StartsWith(e.ToString()));
+                    if (i < t.Length)
+                    {
+                        finalResult.Append(t[i]);
+                    }
+                }
+            }
+
+            return finalResult.ToString().TrimEnd();
+        }
+
+        public  bool isMerge(string s, string part1, string part2)
+        {
+            Console.WriteLine("{0}->{1},{2}", s, part1, part2);
+            if (string.IsNullOrEmpty(s) && string.IsNullOrEmpty(part1) && string.IsNullOrEmpty(part2))
+            {
+                return true;
+            }
+            if (string.IsNullOrEmpty(s) && (!string.IsNullOrEmpty(part1) || !string.IsNullOrEmpty(part2)))
+            {
+                return false;
+            }
+
+
+            var original = s.ToCharArray().ToList();
+            var w1 = part1.ToCharArray().ToList();
+            var w2 = part2.ToCharArray().ToList();
+            var combined = new List<char>();
+            combined.AddRange(w1);
+            combined.AddRange(w2);
+
+            var intersect = original.Where(r => combined.Contains(r));
+            Console.WriteLine("Intersect: {0}, Original: {1}, plain Intersect: {2}", string.Join("", intersect.OrderBy(x => x)), string.Join("", original.OrderBy(x => x)), string.Join("", intersect));
+            if (string.Join("", intersect.OrderBy(x => x)) != string.Join("", original.OrderBy(x => x)))
+            {
+                Console.WriteLine("Word matching Failure");
+                return false;
+            }
+
+            var w1Intersect = original.Where(r => w1.Contains(r)).ToArray();
+
+            for (var i = 0; i < w1Intersect.Count(); i++)
+            {
+                if (w1Intersect[i] != w1[i])
+                {
+                    Console.WriteLine("Order Failure 1");
+                    return false;
+                }
+            }
+
+
+            var w2Intersect = original.Where(r => w2.Contains(r)).ToArray();
+
+            for (var i = 0; i < w2Intersect.Count(); i++)
+            {
+                if (w2Intersect[i] != w2[i])
+                {
+                    Console.WriteLine("Order failure 2");
+                    return false;
+                }
+            }
+
+            return true;
+
+
+        }
+
+        public bool IsAllPossibilities(int[] arr)
+        {
+           // Console.WriteLine(string.Join(",", arr));
+            /* foreach(var a in arr)
+             {Console.WriteLine(a);}*/
+
+            //Console.WriteLine("Over");
+            //Console.WriteLine(arr != null && arr.Length > 0 && !arr.Any(a => a < 0 || a > arr.Length - 1));
+            return arr != null && arr.Length > 0 && !arr.Any(a => a < 0 || a > arr.Length - 1);
+
+        }
+
+        public bool IsAllPossibilities_copied(int[] arr)
+        {
+            var e = Enumerable.Range(0, arr.Length);
+            var r = e.Except(arr);
+
+            return arr.Any() && !Enumerable.Range(0, arr.Length).Except(arr).Any();
+
+            //List<int> list = arr.ToList();
+            //list.Sort();
+            //return (list.Count > 0 && list[0] == 0 && list[list.Count - 1] == list.Count - 1);
+        }
+
+        
 
         public int SumWithOutHighestAndLowestNumber(int[] numbers)
         {
@@ -91,5 +229,23 @@ namespace Katas
             var buyingACar = new[] {counter, Convert.ToInt32(amountSaved + priceOld - priceNew)};
             return buyingACar;
         }
+    }
+
+    class BagelSolver
+    {
+        public static Bagel Bagel
+        {
+            get
+            {
+                // TODO: Implement me
+                return new Bagel() ;
+            }
+
+        }
+    }
+
+    sealed class Bagel
+    {
+        public int Value { get; } = 3;
     }
 }
